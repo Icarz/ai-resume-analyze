cc8836442842d7d8fa36431761ef89d185dfd490
Icarz <mr.rghay@email.com>
2025-10-30 03:08:11 +0100

    starting the auth section

---
+++ app/routes/Auth.tsx
@@ -0,0 +1,43 @@
+import { usePuterStore } from "~/lib/puter";
+
+export const meta = () => [
+  { title: "Resumind | Auth" },
+  { name: "description", content: "log in into your account" },
+];
+const Auth = () => {
+  const { isLoading, auth } = usePuterStore();
+  return (
+    <main className="bg-[url('/images/bg-auth.svg')] bg-cover min-h-screen flex items-center justify-center">
+      <div className="gradient-border shadow-lg">
+        <section className="flex flex-col gap-8 bg-white rounded-2xl p-10">
+          <div className="flex flex-col items-center gap-8 text-center">
+            <h1> Here you are !</h1>
+            <h2>log in to continue your job craft</h2>
+          </div>
+            <div>
+                {" "}
+                {isLoading ? (
+                    <button className="auth-button animate-pulse">
+                        <p> signing you in ...</p>
+                    </button>
+                ) : (
+                    <>
+                        {auth.isAuthenticated ? (
+                            <button className="auth-button" onClick={auth.signOut}>
+                                Log out
+                            </button>
+                        ) : (
+                            <button className="auth-button" onClick={auth.signIn}>
+                                Log in
+                            </button>
+                        )}
+                    </>
+                )}
+            </div>
+        </section>
+      </div>
+
+    </main>
+  );
+};
+export default Auth;
--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,7 +1,6 @@
-
-import type {Route} from "./+types/home"
+import type { Route } from "./+types/home";
 import Navbar from "~/components/Navbar";
-import {resumes} from "../../constants";
+import { resumes } from "../../constants";
 import ResumeCard from "~/components/ResumeCard";
 export function meta({}: Route.MetaArgs) {
   return [
@@ -11,19 +10,22 @@ export function meta({}: Route.MetaArgs) {
 }
 
 export default function Home() {
-  return <main className="bg-[url('/images/bg-main.svg')] bg-cover">
-      <Navbar/>
+  return (
+    <main className="bg-[url('/images/bg-main.svg')] bg-cover">
+      <Navbar />
       <section className="main-section">
-          <div className="page-heading py-16">
-              <h1>track your application &  Resume rating</h1>
-              <h2>review your application and check ai power feedback</h2>
-          </div>
-          {resumes.length > 0 &&(<div className="resumes-section">{resumes.map((resume)=>(
+        <div className="page-heading py-16">
+          <h1>track your application & Resume rating</h1>
+          <h2>review your application and check ai power feedback</h2>
+        </div>
+        {resumes.length > 0 && (
+          <div className="resumes-section">
+            {resumes.map((resume) => (
               <ResumeCard key={resume.id} resume={resume} />
-          ))}</div>)}
+            ))}
+          </div>
+        )}
       </section>
-
-
-
-  </main>;
+    </main>
+  );
 }
